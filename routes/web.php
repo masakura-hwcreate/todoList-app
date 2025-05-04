ad0f655976a9ffd3d8a7939557c929aebebcd38c
masakura-hwcreate <masakura@hw-create.com>
2025-05-04 11:52:59 +0900

    make:contoroller_list,card

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ListController;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('lists', ListController::class);
+
+Route::resource('cards', CardController::class);
+
+require __DIR__.'/auth.php';
